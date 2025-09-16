8dbd439dd199d55aa77c1e6ef39db3ed4519421f
DasalioK <2211524@talnet.nl>
2025-09-16 09:15:31 +0200

    Dasalio-simon

---
+++ getallen2.php
@@ -0,0 +1,11 @@
+<?php
+$mijnLeeftijd = 17;
+$vorigJaar = $mijnLeeftijd - 1;
+$overZesJaar = $mijnLeeftijd + 6;
+
+echo "Ik ben $mijnLeeftijd jaar";
+echo "<br>";
+echo "Vorig jaar was ik $vorigJaar oud";
+echo "<br>";
+echo "Over 6 jaar ben ik $overZesJaar oud";
+?>
